130fca868d9dfe6cb2662a1e5055769d1183a43a
Ben Crist <ben@magicmoremagic.com>
2017-01-08 15:58:54 -0600

    build.lua syntax overhaul

--- build.lua
+++ build.lua
@@ -1,27 +1,22 @@
-tool { name = 'bstyle',
-   projects = {
-      app {
-         icon = 'icon/bengine-warm.ico',
-         rtti = true,
-         custom = {
-            outputs = { 'src/cxx_lexer.cpp' },
-            inputs = { 'meta/cxx_lexer.rl' },
-            name = 'ragel',
-            command = 'ragel $flags -o $out $in',
-            flags = '-s -C -L -G2',
-            generator = true,
-            restat = true
-         },
-         src = {
-            'src/*.cpp'
-         },
-         libs = {
-            'core',
-            'core-id-with-names',
-            'util',
-            'ctable',
-            'cli'
-         }
+tool 'bstyle' {
+   app {
+      icon 'icon/bengine-warm.ico',
+      rtti,
+      custom {
+         outputs = 'src/cxx_lexer.cpp',
+         inputs = 'meta/cxx_lexer.rl',
+         rule = 'ragel',
+         command = 'ragel $flags -o $out $in',
+         flags = '-s -C -L -G2',
+         generator = true,
+         restat = true
+      },
+      link_project {
+         'core',
+         'core-id-with-names',
+         'util',
+         'ctable',
+         'cli'
       }
    }
 }
